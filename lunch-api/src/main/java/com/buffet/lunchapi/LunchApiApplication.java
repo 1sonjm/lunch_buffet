327cb9bc3fb868ebb9ff76e9358d2ed51a044134
JoungMan Son <sonjm1758@gmail.com>
2018-10-17 16:02:07 +0900

    init source

---
+++ lunch-api/src/main/java/com/buffet/lunchapi/LunchApiApplication.java
@@ -0,0 +1,16 @@
+package com.buffet.lunchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// 아래 annotaion 기능
+// @EnableAutoConfiguration: enable Spring Boot’s auto-configuration mechanism
+// @ComponentScan: enable @Component scan on the package where the application is located (see the best practices)
+// @Configuration: allow to register extra beans in the context or import additional configuration classes
+public class LunchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchApiApplication.class, args);
+	}
+}
