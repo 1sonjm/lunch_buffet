7d62f893ea52064a7d07ae24cb009ca7cf034b8d
JoungMan Son <sonjm1758@gmail.com>
2018-10-24 18:58:43 +0900

    pskStore 설정

--- lunch-api/src/main/java/com/buffet/lunchapi/LunchApiApplication.java
+++ lunch-api/src/main/java/com/buffet/lunchapi/LunchApiApplication.java
@@ -5,8 +5,16 @@ import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+@EnableAuthorizationServer
+//Oauth2 권한 서버
+
+@EnableResourceServer
+//API 접근시 인증 권한 설정
 
 @SpringBootApplication
 // 아래 annotaion 기능
--- lunch-api/src/main/java/com/buffet/lunchapi/config/ResourceServerConfig.java
+++ lunch-api/src/main/java/com/buffet/lunchapi/config/ResourceServerConfig.java
@@ -1,14 +1,11 @@
-package com.buffet.lunchapi.configuration;
+package com.buffet.lunchapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
-@EnableResourceServer
-//API 접근시 인증 권한 설정
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     private final Environment env;
