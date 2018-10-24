57528b0dbb243dd50d4300871cb86f0cba5d887e
JoungMan Son <sonjm1758@gmail.com>
2018-10-24 15:38:45 +0900

    설정 수정

--- lunch-api/src/main/java/com/buffet/lunchapi/configuration/AuthorizationServerConfig.java
+++ lunch-api/src/main/java/com/buffet/lunchapi/configuration/AuthorizationServerConfig.java
@@ -1,36 +1,12 @@
 package com.buffet.lunchapi.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 //Oauth2 권한 서버
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-    @Autowired
-    private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private TokenStore JdbcTokenStore;
-
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient("foo")
-                .secret("bar")
-                .authorizedGrantTypes("password", "authorization_code", "refresh_token")
-                .scopes("read");
-    }
-
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-
-        endpoints.tokenStore(JdbcTokenStore).authenticationManager(authenticationManager);
-    }
 }
